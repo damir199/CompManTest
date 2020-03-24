b6d1524ca1aeca778ce2cfea9491dad832cec02d
damir <damir8520@gmail.com>
2020-03-24 11:36:17 -0700

    added getIP() function that fetches ip in hex. need to alter for normal value.

--- Program.cs
+++ Program.cs
@@ -1,12 +1,29 @@
 ﻿using System;
+using System.Net;
 
 namespace CompManTest
 {
     class Program
     {
+        
+
+        public static void getIP()
+        {
+            var host = Dns.GetHostName().ToString();
+            string systemIP;
+
+            systemIP = Dns.GetHostEntry(host).AddressList[0].ToString();
+
+
+
+           Console.WriteLine(systemIP.ToString());
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
-            
+            getIP();
+
         }
 
 
