d6eb5290b00ef2c98d6d7c288ca36be2f074a772
damir <damir8520@gmail.com>
2020-03-25 11:48:46 -0700

    Created a 'DeviceFunctions' Class to hold the functions that will retrieve all of the data from the device.
    Will keep things cleaner as the project gets bigger.

---
+++ DeviceFunctions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+
+namespace CompManTest
+{
+    class DeviceFunctions
+    {
+        public static string getIP()
+        {
+            var host = Dns.GetHostName().ToString();
+            string systemIP;
+
+            //Fethes IPv4 address by using index [1] of AddressList.           
+            //TO-DO: 
+            //Create a loop for addresslist that selects the ipv4 address instead of selecting the index.
+            systemIP = Dns.GetHostEntry(Dns.GetHostName().ToString()).AddressList[1].ToString();
+
+            //                  ----TESTING------
+            Console.WriteLine(systemIP.ToString());
+
+            //return the IP as string to use with rest of program.
+            return systemIP;
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Net;
+using CompManTest;
 
 namespace CompManTest
 {
@@ -7,29 +7,10 @@ namespace CompManTest
     {
         
 
-
-        public static string getIP()
-        {
-            var host = Dns.GetHostName().ToString();
-            string systemIP;
-
-            //Fethes IPv4 address by using index [1] of AddressList.           
-            //TO-DO: 
-            //Create a loop for addresslist that selects the ipv4 address instead of selecting the index.
-            systemIP = Dns.GetHostEntry(Dns.GetHostName().ToString()).AddressList[1].ToString();
-            
-            //                  ----TESTING------
-            Console.WriteLine(systemIP.ToString());
-            
-            //return the IP as string to use with rest of program.
-            return systemIP;
-            
-        }
-
         static void Main(string[] args)
         {
-            getIP();
-
+            DeviceFunctions.getIP();
+           
         }
 
 
