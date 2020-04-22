fe2c30281cb2ed2ce08111db588c03757d1dc456
Damir <damir8520@gmail.com>
2020-04-22 06:29:40 -0700

    Port scanner works on a basic level - Takes an array of ports to check and reports whether the port is open or closed alongside the port number tested.
    Uses TcpClient to attempt a connection.
    
    Hard coded IP to test currently. ROUTER

--- NetFunctions.cs
+++ NetFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Net.Sockets;
+using System.Net.NetworkInformation;
 using System.Collections.Generic;
 using System.Text;
 
@@ -10,45 +11,56 @@ namespace CompManTest
 
         public static void pScan()
         {
+
+
+
+
             Console.Clear();
-            string IP = DeviceFunctions.getIP();
-           // TcpClient scan = new TcpClient();
-            foreach(int s in Ports)
+            string IP = "192.168.0.1";
+            // TcpClient scan = new TcpClient();
+            using(TcpClient scan = new TcpClient())
+            foreach (int s in Ports)
             {
-                using (TcpClient scan = new TcpClient()){
                     try
                     {
                         scan.Connect(IP, s);
-                        Console.WriteLine($"[{s}] | OPEN ");
+                        Console.WriteLine($"[{s}] | OPEN");
                     }
                     catch
                     {
+                        Console.WriteLine($"[{s}] | CLOSED");
+                    }
 
-                        Console.WriteLine($"[{s}] | CLOSED ");
+                
 
-                    } 
-                }
+
             }
-
         }
+                
+            
+
+        
 
 
 
-        private static int[] Ports = new int[]
-        {
-           // 25,
-            //80,
-            //443,
-            //20,
-            //21,
-            //22,
-            //23,
-            //3389,
-            //4200,
-            4201,
-            3000,
+                private static int[] Ports = new int[]
+                {
+                   // 25,
+                    80,
+                    //443,
+                    //20,
+                    //21,
+                    //22,
+                    //23,
+                    //3389,
+                    4200,
+                    8080,
+                    4201,
+                    3000,
 
-        };
+                };
 
     }
+    
 }
+
