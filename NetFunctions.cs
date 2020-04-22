492563de8a3dee7ed2c6f0445aad5e07af5b1843
Damir <damir8520@gmail.com>
2020-04-22 06:40:47 -0700

    create a list of open ports for ue with other parts of program.

--- NetFunctions.cs
+++ NetFunctions.cs
@@ -14,27 +14,31 @@ namespace CompManTest
 
 
 
-
+            var openports = new List<string>();
             Console.Clear();
             string IP = "192.168.0.1";
-            // TcpClient scan = new TcpClient();
             using(TcpClient scan = new TcpClient())
             foreach (int s in Ports)
             {
                     try
                     {
                         scan.Connect(IP, s);
+                        openports.Add("port:" + $"[{s}] | OPEN " + "At IP: " + IP);
                         Console.WriteLine($"[{s}] | OPEN");
+
                     }
                     catch
                     {
+                        openports.Add("port:" + $"[{s}] | CLOSED " + "At IP: " + IP);
                         Console.WriteLine($"[{s}] | CLOSED");
                     }
 
-                
-
-
             }
+            for(int i = 0;  i < openports.Count; i++)
+            {
+                Console.WriteLine(openports[i]);
+            }
+            
         }
                 
             
@@ -45,14 +49,14 @@ namespace CompManTest
 
                 private static int[] Ports = new int[]
                 {
-                   // 25,
+                    25,
                     80,
-                    //443,
-                    //20,
-                    //21,
-                    //22,
-                    //23,
-                    //3389,
+                    443,
+                    20,
+                    21,
+                    22,
+                    23,
+                    3389,
                     4200,
                     8080,
                     4201,
